d2a8efe05322cd57113a16890bbdc2239453c6ad
GiuseppeMP <giuseppemath@gmail.com>
2024-02-11 18:19:03 -0300

    (feat): add i3/config

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 # create .config
 mkdir -p ~/.config
+mkdir -p ~/.i3
 
 # Install links
 ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/starship/starship.toml ~/.config/starship.toml
 ln -sf ~/.dotfiles/kitty ~/.config/kitty
 ln -sf ~/.dotfiles/nvim ~/.config/nvim
+ln -sf ~/.dotfiles/.i3/config ~/.i3/config
